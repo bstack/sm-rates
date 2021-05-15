8ff2a81b789159365a217f707cc5fdd96184c7b5
billy.stack <billy.stack@channeladvisor.com>
2021-05-15 10:28:48 +0100

    Removed db container, no longer used

--- AppSettings.cs
+++ AppSettings.cs
@@ -4,7 +4,6 @@ namespace rates
 {
 	public class AppSettings
 	{
-		public string DatabaseConnectionString { get; private set; }
 		public string RatesRequestUri { get; private set; }
 		public string ReportingRequestUri { get; private set; }
 	}
